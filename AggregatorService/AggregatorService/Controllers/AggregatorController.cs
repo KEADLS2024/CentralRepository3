069afd02b77a06ea85077ddd8aaa44f9908fa987
MikkelYtting <62931295+MikkelYtting@users.noreply.github.com>
2024-05-28 15:33:57 +0200

    aggregate service

--- AggregatorService/AggregatorService/Controllers/AggregatorController.cs
+++ AggregatorService/AggregatorService/Controllers/AggregatorController.cs
@@ -15,24 +15,18 @@ namespace AggregatorService.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        [HttpGet("user-order-catalog")]
-        public async Task<IActionResult> GetUserOrderCatalogData()
+        [HttpGet("order")]
+        public async Task<IActionResult> GetOrderData()
         {
             var client = _httpClientFactory.CreateClient();
 
-            // Call UserService
-            var addressesResponse = await client.GetStringAsync("http://user-app:8060/api/Addresses");
             // Call OrderService
-            var ordersResponse = await client.GetStringAsync("http://order-app:8080/api/OrderTables");
-            // Call CatalogService
-            var categoriesResponse = await client.GetStringAsync("http://catalog-app:8070/api/Categories");
+            var ordersResponse = await client.GetStringAsync("http://order-app/api/OrderTables");
 
             // Aggregate the data
             var aggregatedData = new
             {
-                Addresses = addressesResponse,
-                Orders = ordersResponse,
-                Categories = categoriesResponse
+                Orders = ordersResponse
             };
 
             return Ok(aggregatedData);
